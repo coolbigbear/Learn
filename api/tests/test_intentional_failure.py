5ac88c8b6d061d6419cde4becc6a3cc91de00c39
Michal Czuper <mrczuper@gmail.com>
2026-07-23 11:26:52 +0100

    ci-test: remove intentionally-failing test (cleanup)

--- api/tests/test_intentional_failure.py
+++
@@ -1,4 +0,0 @@
-
-def test_intentional_failure():
-    """This test intentionally fails to verify CI failure detection."""
-    assert False, "This is an intentional failure to test CI failure handling"
